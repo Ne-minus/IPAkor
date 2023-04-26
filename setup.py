627013841b68fcb59c01add4de9d1f1d31fee1fc
Ne-minus <neminova2.0@gmail.com>
2023-04-26 06:42:26 +0300

    changed version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='IPAkor',
-                 version='1.0',
+                 version='1.1',
                  packages=setuptools.find_packages(),
                  include_package_data=True,
                  classifiers=[
